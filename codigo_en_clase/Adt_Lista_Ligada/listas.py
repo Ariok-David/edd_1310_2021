3c43098eca0af71fbf13462b52b94e4542f03b2f
Ariok-David <ariokleon368@aragon.unam.mx>
2020-11-19 12:53:59 -0600

    Creado mediante Atom por ADLM

--- codigo_en_clase/Adt_Lista_Ligada/listas.py
+++ codigo_en_clase/Adt_Lista_Ligada/listas.py
@@ -25,10 +25,38 @@ class LinkedList:
         while curr_node != None:
             print(f"{curr_node.data} -->", end = "")
             curr_node = curr_node.siguiente
+        print(" ")
 
     def remove(self, value):
         curr_node = self.__head
-        while curr_node.data != value and curr_node.siguiente != None:
+        if self.__head.data == value:
+            self.__head = self.__head.siguiente
+        else:
+            anterior = None
+            while curr_node.data != value and curr_node.siguiente != None:
+                anterior = curr_node
+                curr_node = curr_node.siguiente
+            if curr_node.data == value:
+                #print("ACTUAL:", anterior.data)
+                anterior.siguiente = curr_node.siguiente
+            else:
+                    print("El dato no existe en la lista")
+
+    def preppend(self, value):
+        nuevo = Nodo(value, self.__head)
+        self.__head = nuevo
+
+    def tail(self):
+        curr_node = self.__head
+        while curr_node.siguiente != None:
             curr_node = curr_node.siguiente
-        if curr_node == value:
-            
+        return curr_node
+
+    def get(self, posicion = None): #Por defecto regresa el ultimo
+        contador  = 0
+        dato = None
+        if posicion == None:
+            dato = self.tail().data
+        else:
+            pass
+        return dato
