6aa819b659b30244e410073bc5697328722c4fe7
Ariok-David <ariokleon368@aragon.unam.mx>
2020-11-24 12:48:58 -0600

    Creado mediante Atom por ADLM

---
+++ codigo_en_clase/Adt_Lista_Doblemente_Ligada/pruebas_listasdobles.py
@@ -0,0 +1,10 @@
+from ListaDoble import DoubleLinkedList
+
+ld = DoubleLinkedList()
+print("Esta vacia: ", ld.is_empty())
+ld.append(10)
+ld.append(20)
+ld.append(30)
+print(f"La lista tiene {ld.get_size()} elementos")
+ld.transversal()
+ld.reverse_transversal()
