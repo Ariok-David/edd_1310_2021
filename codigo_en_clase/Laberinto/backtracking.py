abf2fe7480164dde866d1e6fae8cf82ea1222ec7
Ariok-David <ariokleon368@aragon.unam.mx>
2020-12-01 12:59:26 -0600

    Creado mediante Atom por ADLM

---
+++ codigo_en_clase/Laberinto/backtracking.py
@@ -0,0 +1,15 @@
+from Array2d import Array2D
+
+class LaberintoADT:
+    # 0 Pasillo
+    # 1 Pared
+    # S salida
+    # E entrada
+    #Pasillos es una tupla((2,1),(2,2),(2,3),(2,4),(3,2),(4,2))
+    def __init__(self, rens, cols, pasillos):
+        self.__laberinto = Array2D(rens, cols, '1')
+        for pasillo in pasillos:
+            self.__laberinto.set_item(pasillo[0],pasillo[1],'0')
+
+    def to_string(self):
+        self.__laberinto.to_string()
