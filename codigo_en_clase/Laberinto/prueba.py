4eb1dc5ace0eedfd95fc3cafaa8519420492235d
Ariok-David <ariokleon368@aragon.unam.mx>
2020-12-03 12:57:17 -0600

    Creado mediante Atom por ADLM

--- codigo_en_clase/Laberinto/prueba.py
+++ codigo_en_clase/Laberinto/prueba.py
@@ -1,5 +1,5 @@
-
+#Creado hoy 1 de diciembre de 2020
 from backtracking import LaberintoADT
 pasillos_inicial = ((2,1),(2,2),(2,3),(2,4),(3,2),(4,2))
-lab = LaberintoADT(6,6,pasillos_inicial)
+lab = LaberintoADT(6,6,pasillos_inicial, (5,2), (2,5)) # Modificado hoy 3 de diciembre de 2020
 lab.to_string()
