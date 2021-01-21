43ba9b24d94e4db3b3a1bc89b7aaa266da228312
Ariok-David <ariokleon368@aragon.unam.mx>
2021-01-21 12:51:08 -0600

    Creado mediante Atom por ADLM

--- codigo_en_clase/Adt Arboles Binarios/arboles_binarios.py
+++ codigo_en_clase/Adt Arboles Binarios/arboles_binarios.py
@@ -1,4 +1,5 @@
 #Creado hoy 19 de Enero de 2021. Grupo : 1310
+#Modificado y terminada hoy 21 de Enero de 2021. Grupo : 1310
 class NodoArbol:
     def __init__(self, value, left = None, right = None):
         self.data = value
@@ -39,13 +40,55 @@ class BinarySearchTree:
             print(nodo.data, end = " , ")
             self.__recorrido_in(nodo.right)
 
+    def __recorrido_pos(self, nodo):
+        if nodo:
+            self.__recorrido_pos(nodo.left)
+            self.__recorrido_pos(nodo.right)
+            print(nodo.data, end = " , ")
+
+    def __recorrido_pre(self, nodo):
+        if nodo:
+            print(nodo.data, end = " , ")
+            self.__recorrido_pre(nodo.left)
+            self.__recorrido_pre(nodo.right)
+
     def transversal(self, format = "inorden"):
         if format == "inorden":
             self.__recorrido_in(self.__root)
         elif format == "preorden":
-            print("recorrido en preorden")
+            self.__recorrido_pre(self.__root)
         elif format == "posorden":
-            print("recorrido en posorden")
+            self.__recorrido_pos(self.__root)
         else:
             print("Error, ese recorrido no existe")
         print("")
+
+    def search(self, value):
+        if self.__root == None:
+            return None
+        else:
+            return self.__search(self.__root, value)
+
+    def __search(self, nodo, value):
+        if nodo == None:
+            print("Caso base")
+            return None
+        elif nodo.data == value:
+            print("Encontrado")
+            return nodo
+        elif value < nodo.data:
+            print("Buscar a la izquierda")
+            return self.__search(nodo.left, value)
+        else:
+            print("Buscar a la derecha")
+            return self.__search(nodo.right, value)
+
+    def remove(self, value):
+        encontrado = self.search(value)
+        #caso 1
+        if encontrado.left == None and encontrado.right == None:
+            print("Eliminando", encontrado.data)
+            encontrado = None
+        #caso 2
+        elif (encontrado.left != None and encontrado.right == None) or (encontrado.left == None and encontrado.right != None):
+            print("A eliminar", encontrado.data)
