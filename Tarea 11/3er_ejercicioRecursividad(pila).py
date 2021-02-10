6215ef524804e422a3c72b116619ee44b78d0498
Ariok León <60637430+Ariok-David@users.noreply.github.com>
2021-02-10 17:02:52 -0600

    Update 3er_ejercicioRecursividad(pila).py
    Corrección

--- Tarea 11/3er_ejercicioRecursividad(pila).py
+++ Tarea 11/3er_ejercicioRecursividad(pila).py
@@ -17,7 +17,7 @@ def pop_stack(pila, num, p_ayuda):
 def find_midle_value(pila):
     p = Stack()
     c = (pila.length() / 2)
-    pop_stack(pila, int(c))
+    pop_stack(pila, int(c),p)
 
 
 def main():
